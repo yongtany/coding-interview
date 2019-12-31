0c65073b000974fc9b4ca8a553e1a5f536b0504d
yongtany <yongn1494@gmail.com>
2019-12-31 15:45:11 +0900

    linked list

--- exercises/weave/index.js
+++ exercises/weave/index.js
@@ -27,12 +27,11 @@ const Queue = require('./queue');
 function weave(sourceOne, sourceTwo) {
   const q = new Queue();
 
-  while (sourceOne.peek() || sourceTwo.peek()) {
-    if (sourceOne.peek()) {
-      q.add(sourceOne.remove());
+  while(sourceOne.peek() || sourceTwo.peek()) {
+    if(sourceOne.peek()) {
+      q.add(sourceOne.remoove());
     }
-
-    if (sourceTwo.peek()) {
+    if(sourceTwo.peek()) {
       q.add(sourceTwo.remove());
     }
   }
